d9b7bf3ace00de0ef9ddc00bf508eedcf043be23
Wright Bagwell <wright@ludaprojects.com>
2023-12-31 22:47:55 -0500

    Refactor pin checks to remove redundant code

--- utils_system.py
+++ utils_system.py
@@ -10,6 +10,17 @@ import board
 
 CPUTILS_STRING = 'CP UTILS:'
 
+# A function to check for a pin
+# Specify the pin and a description of what it does
+# If you're using a board not made by Adafruit, you can use this function to search for non-stnadard pin names.
+def check_for_pin(pin, descriptor):
+    if hasattr(board, pin):
+        pin_detected = True
+    else:
+        pin_detected = False
+    print("\t" + pin, "\t" + descriptor + "\t", pin_detected)
+    return pin_detected
+
 def print_board_info():
     print(CPUTILS_STRING, "Getting all board details...")
 
@@ -23,9 +34,12 @@ def print_board_info():
     get_dotstar_info()
     get_neopixel_info()
     get_lightsensor_info()
+    get_temperature_info()
     get_speakeroutput_info()
+    get_microphone_info()
     get_i2c_info()
-
+    get_uart_info()
+    get_spi_info()
 
 # Show details about storage and memory
 def get_mem_storage_info():
@@ -35,16 +49,10 @@ def get_mem_storage_info():
     free_memory = gc.mem_free() / 1024 / 1024
     print("\tFree memory:\t", f"{free_memory:,}", "\tMB" )
 
-
 # See if the board has a display. If so, show details.
 # Almost always set to board.DISPLAY pin
 def get_display_info():
-    DISPLAY_PIN = 'DISPLAY'
-    if hasattr(board, DISPLAY_PIN):
-        builtin_display = True
-    else:
-        builtin_display = False
-    print("\t" + DISPLAY_PIN, "\t(display):\t", builtin_display)
+    builtin_display = check_for_pin("DISPLAY","onboard display")
 
     if builtin_display:
         display = board.DISPLAY
@@ -72,88 +80,52 @@ def get_display_info():
 # See if there's a simple status LED on the board.
 # Almost always set to board.LED pin
 def get_status_led_info():
-    LED_PIN = 'LED'
-
-    if hasattr(board, LED_PIN):
-        led_detected = True
-    else:
-        led_detected = False
-    print("\t" + LED_PIN, "\t(status LED):\t", led_detected)
+    check_for_pin("LED","status LED\t")
 
 # See if there's a simple status indicator LED on the board.
 # This is typically a requirement for CircuitPython boards,
 # but there may be some out there that don't have one.
 # Almost always set to board.LED pin
 def get_dotstar_info():
-    DOTSTAR_PIN = 'APA102_SCK'
-
-    if hasattr(board, DOTSTAR_PIN):
-        dotstar_detected = True
-    else:
-        dotstar_detected = False
-    print("\t" + DOTSTAR_PIN, "\t(DotStar):\t", dotstar_detected)
+    check_for_pin("APA102_SCK","DotStar LED\t")
 
 # See if there's a Neopixel LED on the board.
 # Almost always set to board.NEOPIXEL pin
 def get_neopixel_info():
+    check_for_pin("NEOPIXEL","Neopixel LED\t")
     NEOPIXEL_PIN = 'NEOPIXEL'
-
-    if hasattr(board, NEOPIXEL_PIN):
-        neopixel_detected = True
-    else:
-        neopixel_detected = False
-    print("\t"+ NEOPIXEL_PIN, "\t(Neopixel):\t", neopixel_detected)
 
 # See if there's a DotStar LED on the board.
 # Almost always set to board.LIGHT pin,
 # though I've seen some called AMB (like Unexpected Maker S2)
 def get_lightsensor_info():
-    LIGHTSENSOR_PIN = 'LIGHT'
-
-    if hasattr(board, LIGHTSENSOR_PIN):
-        lightsensor_detected = True
-    else:
-        lightsensor_detected = False
-    print("\t" + LIGHTSENSOR_PIN, "\t(Light sensor):\t", lightsensor_detected)
+    check_for_pin("LIGHT","Ambient light sensor")
+    check_for_pin("AMB","Ambient light sensor")
 
 # See if there's a Temperature sensot on the board.
 # Almost always set to board.TEMPERATURE pin,
-def get_lightsensor_info():
-    TEMPERATURE_PIN = 'TEMPERATURE'
-
-    if hasattr(board, TEMPERATURE_PIN):
-        temperature_sensor_detected = True
-    else:
-        temperature_sensor_detected = False
-    print("\t" + TEMPERATURE_PIN, "\t(temperature sensor):\t", temperature_sensor_detected)
+def get_temperature_info():
+    check_for_pin("TEMPERATURE","temperature sensor")
 
 # See if there's a speaker output on the board.
 # Almost always set to board.SPEAKER pin
 def get_speakeroutput_info():
-    SPEAKER_PIN = 'SPEAKER'
+   check_for_pin("SPEAKER", "Analog speaker output")
 
-    if hasattr(board, SPEAKER_PIN):
-        speakeroutput_detected = True
-    else:
-        speakeroutput_detected = False
-    print("\t" + SPEAKER_PIN, "\t(speaker ouput):\t", speakeroutput_detected)
+# See if there's microphone on the board.
+# Almost always set to board.MICROPHONE_CLOCK and MICROPHONE_DATA, which comes in pairs.
+def get_microphone_info():
+    check_for_pin("MICROPHONE_CLOCK", "Microphone")
 
-# See if there's an I2C Stemma QT connenctor on the board.
+# See if there's an I2C/STEMMA QT connector on the board.
 # Almost always set to board.I2C
 # Of course you can also manually create I2C connections
 # this does not verify whether you can or that you have.
 def get_i2c_info():
-    I2C_PIN = 'I2C'
-
-    if hasattr(board, I2C_PIN):
-        i2c_detected = True
-    else:
-        i2c_detected = False
-    print("\t" + I2C_PIN, "\t(I2C/STEAMMA connector):\t", i2c_detected)
+    i2c_detected = check_for_pin("I2C", "I2C/STEMMA QT")
     if i2c_detected:
         return
         get_i2c_device_addresses()
-
 
 def get_i2c_device_addresses():
     i2c = board.I2C()
@@ -168,3 +140,14 @@ def get_i2c_device_addresses():
         for address in i2c_addresses:
             print("\t" + hex(address))
     i2c.unlock()
+    print("\tI2C scan complete")
+
+# See if there's UART on the board.
+# Almost always set to board.UART
+def get_uart_info():
+    check_for_pin("UART", "UART pin\t")
+
+# See if there's SPI on the board.
+# Almost always set to board.SPI
+def get_spi_info():
+    check_for_pin("SPI", "SPI pin\t")
