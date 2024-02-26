1a7850fbc964376c0047a2518818aee49a3a0ae6
Wright Bagwell <wright@ludaprojects.com>
2024-02-25 21:54:34 -0800

    added rotate function

--- utils_display.py
+++ utils_display.py
@@ -5,14 +5,13 @@
 
 """ Helper functions for your board's buit-in display """
 
+import board
 
 # See if the board has a display. If so, show details.
 # Almost always set to board.DISPLAY pin
 def get_display_info():
     """Check for a builtin or onboard display, and show its details.
     Will not show details about a display that you add yourself."""
-
-    import board
 
     print("\n=== Built-in Display info ===\n")
 
@@ -28,17 +27,27 @@ def get_display_info():
         )
         print(f"{"rotation":<20}", display.rotation)
         print(f"{"bus":<20}", display.bus)
+        
+        # Auto-refresh
         # Some displays, like epaper, do not have an auto-refresh property
         if hasattr(display, 'auto_refresh'):
             auto_refresh_attribute = display.auto_refresh
         else:
             auto_refresh_attribute = "None (probably e-ink)"
         print(f"{"auto_refresh":<20}", auto_refresh_attribute)
+        
+        # Brightness
         # Some displays, like epaper, do not have a brightness property
         if hasattr(display, 'brightness'):
             brightness_attribute = display.brightness
         else:
             brightness_attribute = "None (probably e-ink)"
         print(f"{"brightness":<20}", brightness_attribute)
+    
     else:
         print("board.DISPLAY pin not found")
+    
+def rotate_display(angle):
+    display = board.DISPLAY
+    display.rotation = angle
+    print("\nDisplay rotated to", angle)
